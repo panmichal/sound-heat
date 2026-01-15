57251d442f58b23ef68e7d9ee5f85fc64cc248c0
Michał Strzelczyk <817457+panmichal@users.noreply.github.com>
2026-01-15 20:18:51 +0100

    Use multiple processors

--- src/filter.rs
+++ src/filter.rs
@@ -1,14 +1,22 @@
-pub struct LowPassFilterState {
+use crate::source::BlockProcessor;
+
+pub struct LowPassFilterBlockProcessor {
     pub prev: f32,
     pub cutoff: f32,
     pub sample_rate: u32,
 }
 
-pub fn low_pass_filter_fn(input: f32, state: &mut LowPassFilterState) -> f32 {
-    let rc = 1.0 / (2.0 * std::f32::consts::PI * state.cutoff);
-    let dt = 1.0 / state.sample_rate as f32;
-    let alpha = dt / (rc + dt);
-    let output = alpha * input + (1.0 - alpha) * state.prev;
-    state.prev = output;
-    output
+impl BlockProcessor for LowPassFilterBlockProcessor {
+    fn block_size(&self) -> usize {
+        1
+    }
+
+    fn process_sample(&mut self, input: f32) -> Option<f32> {
+        let rc = 1.0 / (2.0 * std::f32::consts::PI * self.cutoff);
+        let dt = 1.0 / self.sample_rate as f32;
+        let alpha = dt / (rc + dt);
+        let output = alpha * input + (1.0 - alpha) * self.prev;
+        self.prev = output;
+        Some(output)
+    }
 }
